65004366c25330c14e2541841f013b30e49d9f7b
doneni <xxerrenn@icloud.com>
2024-02-13 04:21:04 +0900

    6749

---
+++ 6749/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int a = 0, b = 0;
+    cin >> a >> b;
+    cout << 2 * b - a << endl;
+
+    return 0;
+}
