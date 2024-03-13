c836a038b65a212b9b12e0f9e977868b42aee088
doneni <xxerrenn@icloud.com>
2024-03-13 15:28:01 +0900

    15655

---
+++ 15655/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 9
+using namespace std;
+
+int N = 0, M = 0;
+int in[MAX] = { 0, };
+int arr[MAX] = { 0, };
+void dfs(int idx)
+{
+    if(idx == M)
+    {
+        for(int i = 0; i < M; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= N; i++)
+    {
+        if(idx && in[i] <= arr[idx - 1])
+            continue;
+        arr[idx] = in[i];
+        dfs(idx + 1);
+    }
+}
+
+int main(void)
+{
+    cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+
+    cin >> N >> M;
+    for(int i = 1; i <= N; i++)
+        cin >> in[i];
+    sort(in + 1, in + 1 + N);
+
+    dfs(0);
+
+    return 0;
+}
