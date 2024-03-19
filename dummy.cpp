3de7cebc35f9d04e7308f40d0b36485f9fab5df6
doneni <xxerrenn@icloud.com>
2024-03-19 14:41:44 +0900

    update dummy.cpp

--- dummy.cpp
+++ dummy.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int main(void)
 {
-    cin.tie(NULL); cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
 
     return 0;
 }
