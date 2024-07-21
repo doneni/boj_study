6b20861dff3537b0deed70e2c27e8abc4b7eb707
doneni <xxerrenn@icloud.com>
2024-07-22 01:42:23 +0900

    18312

--- 18312/main.cpp
+++ 18312/main.cpp
@@ -8,21 +8,21 @@ int main()
     int ans = 0;
     for(int h = 0; h <= N; h++)
     {
-        if(h % 10 == K || h / 10 == K || (K == 0 && h < 10))
+        if(h % 10 == K || h / 10 == K)
         {
             ans += 60 * 60;
             continue;
         }
         for(int m = 0; m < 60; m++)
         {
-            if(m % 10 == K || m / 10 == K || (K == 0 && m < 10))
+            if(m % 10 == K || m / 10 == K)
             {
                 ans += 60;
                 continue;
             }
             for(int s = 0; s < 60; s++)
             {
-                if(s % 10 == K || s / 10 == K || (K == 0 && s < 10))
+                if(s % 10 == K || s / 10 == K)
                     ans++;
             }
         }
