acb62786d2cb1e20d0412d9ff9d33452f22a3cfc
doneni <xxerrenn@icloud.com>
2024-11-05 20:50:38 +0900

    10870

---
+++ 10870/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N; cin >> N;
+    int dp[N+1] = { 0, };
+    dp[1] = 1;
+    for(int i = 2; i <= N; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    cout << dp[N];
+    return 0;
+}
